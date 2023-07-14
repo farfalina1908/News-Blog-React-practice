c8ec33745d14f4368aff69b7db03ca72836853e6
Elena Berezina <farfalina1908@gmail.com>
2023-07-14 13:47:21 +0300

    Update Notfoundpage.jsx

--- src/pages/Notfoundpage.jsx
+++ src/pages/Notfoundpage.jsx
@@ -1,12 +1,19 @@
-
-import { CustomLink } from "../components/CustomLink"
+// import { CustomLink } from '../components/CustomLink';
+import { Link } from 'react-router-dom';
 
 const Notfoundpage = () => {
-   return (
-      <div>
-         This page doesn't exist. Go <CustomLink to="/">home</CustomLink>
-      </div>
-   )
-}
+  return (
+    <div>
+      {/* This page doesn't exist. Go <CustomLink to="/">home</CustomLink> */}
+      This page doesn't exist. Go{' '}
+      <Link
+        style={{ textDecoration: 'underline', color: 'var(--color-active)' }}
+        to="/"
+      >
+        home
+      </Link>
+    </div>
+  );
+};
 
-export { Notfoundpage }
+export { Notfoundpage };
